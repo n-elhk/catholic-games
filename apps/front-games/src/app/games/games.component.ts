5939afbf4058bff458000e9c7f9b6d259f663ef3
Nicolas Hayek <hayeknico@hotmail.fr>
2023-11-11 14:06:08 +0100

    create card component

--- apps/front-games/src/app/games/games.component.ts
+++ apps/front-games/src/app/games/games.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from '../components/card/card.component';
 
 @Component({
   selector: 'catholic-games-games',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardComponent],
   templateUrl: './games.component.html',
   styleUrls: ['./games.component.scss'],
 })
